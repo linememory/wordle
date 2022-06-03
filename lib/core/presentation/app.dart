710eb21c39bd878437925b3f479dd73c9ec5f153
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-03 12:16:38 +0200

    added main app widget

---
+++ lib/core/presentation/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:wordle/core/presentation/routes/app_router.gr.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _appRouter = AppRouter();
+    return MaterialApp.router(
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wordle/core/presentation/app.dart';
 
 void main() {
-  runApp(Container());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
