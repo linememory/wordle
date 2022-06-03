9440430dff4856edcc9457b5c547600a781aca6f
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-03 12:15:47 +0200

    added wordle page

---
+++ lib/wordle/presentation/wordle_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WordlePage extends StatelessWidget {
+  const WordlePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Wordle",
+        ),
+      ),
+    );
+  }
+}
