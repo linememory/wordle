9401b4d19177f85d2e2eae1787c8ebb38b15b822
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-04 11:10:32 +0200

    added providers

---
+++ lib/wordle/shared/providers.dart
@@ -0,0 +1,22 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wordle/wordle/application/wordle_notifier.dart';
+import 'package:wordle/wordle/infrastructure/words_datasource.dart';
+import 'package:wordle/wordle/infrastructure/words_repository.dart';
+
+final assetPathProvider = Provider<String>((ref) {
+  return 'assets/words.json';
+});
+
+final wordsDatasourceProvider = Provider<WordsDatasource>((ref) {
+  return JsonWordsDatasource(ref.watch(assetPathProvider));
+});
+
+final wordsRepositoryProvider = Provider<WordsRepository>((ref) {
+  return WordsRepository(ref.watch(wordsDatasourceProvider));
+});
+
+final wordleProvider = StateNotifierProvider<WorldeNotifier, WordleState>(
+  (ref) {
+    return WorldeNotifier(ref.watch(wordsRepositoryProvider));
+  },
+);
