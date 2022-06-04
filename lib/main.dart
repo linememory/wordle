c91cff1dfbb8e363130edb473676362e660501ff
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-04 10:52:54 +0200

    ensure widget flutter bindings are initialized

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wordle/core/presentation/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     const ProviderScope(
       child: App(),
