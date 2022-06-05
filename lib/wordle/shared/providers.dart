c0c058f982cd28601c1da93ea1fb26dfbf05ded1
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-05 19:58:34 +0200

    added dictionary files

--- lib/wordle/shared/providers.dart
+++ lib/wordle/shared/providers.dart
@@ -1,14 +1,16 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wordle/wordle/application/wordle_notifier.dart';
+import 'package:wordle/wordle/infrastructure/dictionary_datasource.dart';
+import 'package:wordle/wordle/infrastructure/dictionary_repository.dart';
 import 'package:wordle/wordle/infrastructure/words_datasource.dart';
 import 'package:wordle/wordle/infrastructure/words_repository.dart';
 
-final assetPathProvider = Provider<String>((ref) {
-  return 'assets/en.txt';
+final wordsAssetPathProvider = Provider<String>((ref) {
+  return 'assets/words/en.txt';
 });
 
 final wordsDatasourceProvider = Provider<WordsDatasource>((ref) {
-  return FileWordsDatasource(ref.watch(assetPathProvider));
+  return FileWordsDatasource(ref.watch(wordsAssetPathProvider));
 });
 
 final wordsRepositoryProvider = Provider<WordsRepository>((ref) {
@@ -17,6 +19,19 @@ final wordsRepositoryProvider = Provider<WordsRepository>((ref) {
 
 final wordleProvider = StateNotifierProvider<WorldeNotifier, WordleState>(
   (ref) {
-    return WorldeNotifier(ref.watch(wordsRepositoryProvider));
+    return WorldeNotifier(ref.watch(wordsRepositoryProvider),
+        ref.watch(dictionaryRepositoryProvider));
   },
 );
+
+final dictionaryAssetPathProvider = Provider<String>((ref) {
+  return 'assets/dictionaries/en.txt';
+});
+
+final dictionaryDatasourceProvider = Provider<DictionaryDatasource>((ref) {
+  return FileDictionaryDatasource(ref.watch(dictionaryAssetPathProvider));
+});
+
+final dictionaryRepositoryProvider = Provider<DictionaryRepository>((ref) {
+  return DictionaryRepository(ref.watch(dictionaryDatasourceProvider));
+});
