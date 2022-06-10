fbc9af3d5def4d94e626eabb27796001e68a2192
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-10 14:47:41 +0200

    disabled landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wordle/core/presentation/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
   runApp(
     const ProviderScope(
       child: App(),
