0f8095a609e3e5e8afa32cf3138b2ddd5a80ae64
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-11 14:56:00 +0200

    added intl

--- lib/core/presentation/app.dart
+++ lib/core/presentation/app.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 import 'package:wordle/core/presentation/routes/app_router.gr.dart';
+import 'package:wordle/generated/l10n.dart';
+import 'package:wordle/settings/domain/settings_notifier.dart';
 import 'package:wordle/settings/shared/providers.dart';
 import 'package:wordle/statistics/shared/providers.dart';
+
+final appRouterProvider = Provider<AppRouter>((ref) {
+  return AppRouter();
+});
 
 class App extends ConsumerWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final _appRouter = AppRouter();
     ref.read(settingsProvider.notifier).loadSettings();
     ref.read(statisticsProvider.notifier).loadStatistics();
-    _appRouter.pushAndPopUntil(
-      const WordleRoute(),
-      predicate: (route) => false,
-    );
+    final AppRouter appRouter = ref.watch(appRouterProvider);
+    final SettingsState settings = ref.watch(settingsProvider);
+
     return MaterialApp.router(
       theme: ThemeData.dark(),
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: appRouter.delegate(),
+      routeInformationParser: appRouter.defaultRouteParser(),
+      locale: Locale(settings.language),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
     );
   }
 }
